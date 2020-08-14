99e9ef5ac0abd932e438cf88f938baa55dbf28b7
nasiegel88 <nasiegel@ucdavis.edu>
2020-08-13 20:44:02 -0700

    Update download-classifier.sh

--- database/download-classifier.sh
+++ database/download-classifier.sh
@@ -3,7 +3,7 @@
 # As of 2020-08-12 the Silva database is reguarly curated
 
 # Silva 138 99% OTUs full-length sequences
-curl -O -J -L https://data.qiime2.org/2020.6/common/silva-138-99-nb-classifier.qza
+curl -O -J -L https://data.qiime2.org/2019.4/common/silva-132-99-nb-classifier.qza
 
 # Silva 138 SSURef NR99 full-length sequences
 curl -O -J -L https://data.qiime2.org/2020.6/common/silva-138-99-seqs.qza
