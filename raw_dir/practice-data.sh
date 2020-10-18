0d7655cc614677722a91f525c46f6ecbb15845cf
Noah Siegel <50184544+nasiegel88@users.noreply.github.com>
2020-10-17 17:12:56 -0700

    Update practice-data.sh

--- raw_dir/practice-data.sh
+++ raw_dir/practice-data.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-
 ## Generatate tar.gz file with all files in current directory
 # tar cf file.tar.gz *.gz
-
 # Preliminary sequencing run
 curl -O -J -L https://osf.io/7uzjb/download
 tar xf file.tar.gz
