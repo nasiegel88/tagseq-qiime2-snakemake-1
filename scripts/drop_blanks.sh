469e704b1b4db027ce3973b23dffaa3909692adf
nasiegel88 <nasiegel@ucdavis.edu>
2020-11-04 16:13:00 -0800

    make drop blank bash script

---
+++ scripts/drop_blanks.sh
@@ -0,0 +1,20 @@
+ var=(config["remove_blanks"])
+if [ "${{var}}" == 'yes' ]; then
+
+    qiime feature-table filter-samples \
+        –i-table {input.table} \
+        –m-metadata-file {config[metadata]} \
+        --p-exclude-ids TRUE \
+        --p-where config["blanks"] \ 
+        –o-filtered-table {output.cleaned_table} 
+
+elif [ "${{var}}" == 'no' ]; then
+
+    qiime feature-table filter-samples \
+        –i-table {input.table} \
+        –m-metadata-file {config[metadata]} \
+        --p-exclude-ids FALSE \ 
+        –o-filtered-table {output.cleaned_table} 
+else 
+
+fi
