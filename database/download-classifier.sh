8249b4caacb3716e15c135a5f44e87db00b0f5dd
Noah Siegel <nasiegel@ucdavis.edu>
2020-11-06 15:41:47 -0800

    Update download-classifier.sh

--- database/download-classifier.sh
+++ database/download-classifier.sh
@@ -10,7 +10,7 @@
 # 2020-11-06
 
 # Silva 138 SSURef NR99 full-length sequences
-curl -sL "https://data.qiime2.org/2020.6/common/silva-138-99-seqs.qza" "silva-138-99-seqs.qza"
+curl -L "https://data.qiime2.org/2020.6/common/silva-138-99-seqs.qza" -o "silva-138-99-seqs.qza"
 
 # Silva 138 SSURef NR99 full-length taxonomy
-curl -sL "https://data.qiime2.org/2020.6/common/silva-138-99-tax.qza" "silva-138-99-tax.qza"
+curl -L "https://data.qiime2.org/2020.6/common/silva-138-99-tax.qza" -o "silva-138-99-tax.qza"
