0cd2db96b87bfce83c8211f2ed82116c88643a79
Noah Siegel <nasiegel@ucdavis.edu>
2020-11-06 15:37:26 -0800

    update database

--- database/download-classifier.sh
+++ database/download-classifier.sh
@@ -2,13 +2,19 @@
 # Download classifier, reprentative sequences, and taxonomy
 # As of 2020-08-12 the Silva database is reguarly curated
 
-# Silva 138 99% OTUs full-length sequences
-curl -L https://data.qiime2.org/2019.10/common/silva-132-99-nb-classifier.qza -o silva-132-99-nb-classifier.qza
-
 # Silva 138 SSURef NR99 full-length sequences
-curl -L https://data.qiime2.org/2020.6/common/silva-138-99-seqs.qza -o silva-138-99-seqs.qza
+#curl -L https://data.qiime2.org/2020.6/common/silva-138-99-seqs.qza -o silva-138-99-seqs.qza
 
 # Silva 138 SSURef NR99 full-length taxonomy
-curl -L https://data.qiime2.org/2020.6/common/silva-138-99-tax.qza -o silva-138-99-tax.qza
+#curl -L https://data.qiime2.org/2020.6/common/silva-138-99-tax.qza -o silva-138-99-tax.qza
 # 2020-11-06
 
+# Silva 138 SSURef NR99 full-length sequences
+curl -sL \
+  "https://data.qiime2.org/2020.6/common/silva-138-99-seqs.qza" > \
+  "silva-138-99-seqs.qza"
+
+# Silva 138 SSURef NR99 full-length taxonomy
+curl -sL \
+  "https://data.qiime2.org/2020.6/common/silva-138-99-tax.qza" > \
+  "silva-138-99-tax.qza"
