7cd81ae7983f057acd191ceafeb9be417621a374
Noah Siegel <nasiegel@ucdavis.edu>
2020-11-06 16:35:22 -0800

    Update download-classifier.sh

--- database/download-classifier.sh
+++ database/download-classifier.sh
@@ -3,5 +3,3 @@
 # As of 2020-08-12 the Silva database is reguarly curated
 # Silva 138 SSURef NR99 full-length sequences
 wget -O "silva-132-99-nb-classifier.qza" "https://data.qiime2.org/2019.4/common/silva-132-99-nb-classifier.qza" --no-hsts 
-# Silva 138 SSURef NR99 full-length taxonomy
-wget -O "silva-132-99-nb-classifier.qza" "https://data.qiime2.org/2019.10/common/silva-132-99-nb-classifier.qza" --no-hsts 
