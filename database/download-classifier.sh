a7878126ae200a78aae6c8aa15c517f73c39d1a8
Noah Siegel <nasiegel@ucdavis.edu>
2020-11-08 12:03:54 -0800

    correct ref

--- database/download-classifier.sh
+++ database/download-classifier.sh
@@ -2,4 +2,4 @@
 # Download classifier, reprentative sequences, and taxonomy
 # As of 2020-08-12 the Silva database is reguarly curated
 # Silva 138 SSURef NR99 full-length sequences
-wget -O "silva-132-99-nb-classifier.qza" "https://data.qiime2.org/2019.4/common/silva-132-99-nb-classifier.qza" --no-hsts 
+wget -O "silva-132-99-nb-classifier.qza" "https://data.qiime2.org/2019.10/common/silva-132-99-nb-classifier.qza" --no-hsts 
