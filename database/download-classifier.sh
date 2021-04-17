b3bf23a495dc3fcfc82094228ebe5fc0598d0912
nasiegel88 <nasiegel@ucdavis.edu>
2021-04-17 12:30:00 -0700

    update example data and classifier

--- database/download-classifier.sh
+++ database/download-classifier.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+
 # Download classifier, reprentative sequences, and taxonomy
 # As of 2020-08-12 the Silva database is reguarly curated
-
 
 ## Silva 138 SSURef NR99 full-length sequences-2019.10
 #curl -sL "https://data.qiime2.org/2019.10/common/silva-138-99-nb-classifier.qza" > "silva-138-99-nb-classifier_2019.10.qza"
