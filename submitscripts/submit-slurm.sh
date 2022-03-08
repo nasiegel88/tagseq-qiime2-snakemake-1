53ac04e7c137c578f88e0be79405e625fc979967
nasiegel88 <nasiegel@ucdavis.edu>
2022-03-08 14:31:01 -0800

    update

--- submitscripts/submit-slurm.sh
+++ submitscripts/submit-slurm.sh
@@ -1,17 +1,19 @@
 #!/bin/bash -login
 #SBATCH -p high                # partition, or queue, to assign to
-#SBATCH -J mld                 # name for job
+#SBATCH -J mld-testing                 # name for job
 #SBATCH -N 1                   # one "node", or computer
 #SBATCH -n 2                   # one task for this node
 #SBATCH -c 4                   # one core per task
 #SBATCH -t 72:00:00             # ask for no more than 30 minutes
-#SBATCH --mem=40Gb             # 20Gb should be enough
+#SBATCH --mem=60Gb             # 20Gb should be enough
+#SBATCH --mail-user=nasiegel@ucdavis.edu        # email address to recieve notification
+#SBATCH --mail-type=ALL                         # NOTIFICATIONS OF SLURM JOB STATUS
 
 # initialize conda
 . ~/miniconda3/etc/profile.d/conda.sh
 
 # activate your desired conda environment
-conda activate snake-tagseq
+conda activate snakemake
 
 # go to the directory you ran 'sbatch' in, OR just hardcode it...
 
@@ -22,8 +24,8 @@ set -x
 
 # run the snakemake!
 # Select which snakefile you want to submit
-cd /home/nasiegel/MLD/tagseq-qiime2-snakemake-1
-snakemake --use-conda
+cd /home/nasiegel/2022-tagseq-qiime2-snakemake-1
+snakemake --use-conda -j 20
 
 # print out various information about the job
 env | grep SLURM            # Print out values of the current jobs SLURM environment variables
